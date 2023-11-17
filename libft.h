ddf773df0dccd69fc0061370a636c9989d519f79
Yuki Jinnouchi <yjinnouc@c4a8c5.42berlin.de>
2023-11-17 17:14:11 +0100

    add prottype to libft.h

--- libft.h
+++ libft.h
@@ -1,3 +1,42 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_open_file(int *file, const char *file_dir, int stdinput);
+
+// int ft_isalpha()
+// int	ft_isdigit()
+// int	ft_isalnum()
+// int ft_isascii()
+// int ft_isprin()
+// int ft_strlen()
+// int ft_memse()
+// int ft_bzero()
+// int ft_memcpy()
+// int ft_memmove()
+// int ft_strlcpy()
+// int ft_strlcat()
+// int ft_toupper()
+// int ft_tolowe()
+// int ft_strchr()
+// int ft_strrchr()
+// int ft_strncm()
+// int ft_memchr()
+// int ft_memcmp()
+// int ft_strn()r()
+// int ft_atoi()
+// int ft_calloc()
+// int ft_strdup()
+
+#endif
+
+/*
+# LIBRARIES
+unistd id for write(), read(), close()
+stdio.h is for printf()
+stdlib.h is for malloc(), free(), exit()
+fcntl.h is for open()
+*/
+
 
 // typedef struct s_list
 // {
