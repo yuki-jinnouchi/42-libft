7c6233b4dae6b6b75f0cd1fb983adccaebe5d2c5
Yuki Jinnouchi <yjinnouc@c4a11c1.42berlin.de>
2023-11-20 23:15:12 +0100

    update

--- libft.h
+++ libft.h
@@ -1,42 +1,33 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int		ft_open_file(int *file, const char *file_dir, int stdinput);
+// # include <unistd.h>
 
-// int ft_isalpha()
-// int	ft_isdigit()
-// int	ft_isalnum()
-// int ft_isascii()
-// int ft_isprin()
-// int ft_strlen()
-// int ft_memse()
-// int ft_bzero()
-// int ft_memcpy()
-// int ft_memmove()
-// int ft_strlcpy()
-// int ft_strlcat()
-// int ft_toupper()
-// int ft_tolowe()
-// int ft_strchr()
-// int ft_strrchr()
-// int ft_strncm()
-// int ft_memchr()
-// int ft_memcmp()
-// int ft_strn()r()
-// int ft_atoi()
-// int ft_calloc()
-// int ft_strdup()
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *s);
+void *ft_memset(void *s, int c, size_t n);
+void ft_bzero(void *s, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
+size_t ft_strlcat(char *dst, const char *src, size_t size);
+int ft_toupper(int c);
+int ft_tolower(int c);
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+void *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+char *ft_strnstr(const char *big, const char *little, size_t len);
+int ft_atoi(const char *nptr);
+void *ft_calloc(size_t nmemb, size_t size);
+char *ft_strdup(const char *s);
 
 #endif
-
-/*
-# LIBRARIES
-unistd id for write(), read(), close()
-stdio.h is for printf()
-stdlib.h is for malloc(), free(), exit()
-fcntl.h is for open()
-*/
-
 
 // typedef struct s_list
 // {
