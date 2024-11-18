37a1ace75445fe1f0f4e574d948d833f6b33bbd9
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2024-11-18 15:08:54 +0000

    fix header and Makefile to adjust new functions

--- Makefile
+++ Makefile
@@ -1,25 +1,28 @@
+# $@: name of the target file (one before colon)
+# $<: name of first prerequisite file (first one after colon)
+# $^: names of all prerequisite files (space separated)
+# $*: stem (bit which matches the % wildcard in rule definition)
+#
+# VAR = val: Normal setting - values within are recursively expand when var used.
+# VAR := val: Setting of var with simple expansion of values inside - values are expanded at decl time.
+# VAR ?= val: Set var only if it doesn't have a value.
+# VAR += val: Append val to existing value (or set if var didn't exist).
+
 # Program name and output directory
 NAME = libft.a
 OUTPUT_DIR = .
 
 # Other directories
-SOURCES_DIR = .
-BONUS_DIR = .
-INCLUDES_DIR = .
+INCLUDES_DIR = 		incl
+SOURCES_ROOT_DIR =	srcs
+SOURCES_DIR :=		$(wildcard $(SOURCES_ROOT_DIR)/*)
+OBJECTS_ROOT_DIR =	objs
+OBJECTS_DIR :=		$(patsubst $(SOURCES_ROOT_DIR)/%,$(OBJECTS_ROOT_DIR)/%,$(SOURCES_DIR))
 
 # Files
-SOURCES = \
-	$(filter-out \
-		$(wildcard $(SOURCES_DIR)/ft_ls*.c), \
-		$(wildcard $(SOURCES_DIR)/*.c))
-OBJECTS =	$(SOURCES:.c=.o)
-
-# BONUS_SOURCES = \
-# 	$(wildcard $(BONUS_DIR)/ft_ls*.c)
-# BONUS_OBJECTS = 	$(BONUS_SOURCES:.c=.o)
-# BONUS_OBJECTS += 	$(OBJECTS) 
-
 HEADERS =	$(INCLUDES_DIR)/libft.h
+SOURCES = 	$(wildcard $(SOURCES_ROOT_DIR)/*/*.c)
+OBJECTS :=	$(patsubst $(SOURCES_ROOT_DIR)/%.c,$(OBJECTS_ROOT_DIR)/%.o,$(SOURCES))
 
 # Compiler and options
 CC = cc
@@ -27,26 +30,47 @@ CFLAGS += -Wall -Wextra -Werror
 DEST = $(OUTPUT_DIR)
 
 # Phony targets
-.PHONY: all clean fclean re aclean ar bonus
+.PHONY: all clean fclean re aclean test
 
 # Targets
 all: $(NAME)
 $(NAME): $(OBJECTS)
 	@echo "----------------"
-	@echo "make OBJECTS done.\n"
+	@echo "make $(NAME) OBJECTS done.\n"
 	ar rcs $@ $(OBJECTS)
 	@echo "----------------"
 	@echo "make $(NAME) done.\n"
-%.o: %.c
-	$(CC) $(CFLAGS) -h $(HEADERS) -c $< -o $@
+
+# Compile Targets
+$(OBJECTS_ROOT_DIR)/%.o: $(SOURCES_ROOT_DIR)/%.c | $(OBJECTS_DIR)
+	$(CC) $(CFLAGS) -I $(INCLUDES_DIR) -c $< -o $@
+$(OBJECTS_DIR):
+	mkdir -p $(OBJECTS_ROOT_DIR)
+	mkdir -p $(OBJECTS_DIR)
+
 clean:
-	rm -f $(OBJECTS)
+	rm -rf $(OBJECTS_ROOT_DIR)
 fclean: clean
 	rm -f $(OUTPUT_DIR)/$(NAME)
 re: fclean all
+
+# non mandatory
 aclean: all clean
-ar: 
+ar:
 	ar rcs $(OUTPUT_DIR)/$(NAME) $(OBJECTS)
-# bonus:
-# 	all $(BONUS_OBJECTS)
-# 	ar rcs $(NAME) $(BONUS_OBJECTS)
+test:
+	@echo "NAME:             $(NAME)"
+	@echo "OUTPUT_DIR:       $(OUTPUT_DIR)"
+	@echo "SOURCES_ROOT_DIR: $(SOURCES_ROOT_DIR)"
+	@echo "SOURCES_DIR:      $(SOURCES_DIR)"
+	@echo "OBJECTS_ROOT_DIR: $(OBJECTS_ROOT_DIR)"
+	@echo "OBJECTS_DIR:      $(OBJECTS_DIR)"
+	@echo "HEADERS:          $(HEADERS)"
+	@echo ""
+	@echo "SOURCES:\n$(SOURCES)"
+	@echo ""
+	@echo "OBJECTS:\n$(OBJECTS)"
+	@echo ""
+	@echo "CC: $(CC)"
+	@echo "CFLAGS: $(CFLAGS)"
+	@echo "DEST: $(DEST)"
--- incl/libft.h
+++ incl/libft.h
@@ -6,7 +6,7 @@
 /*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 21:27:21 by yjinnouc          #+#    #+#             */
-/*   Updated: 2023/12/02 12:49:56 by yjinnouc         ###   ########.fr       */
+/*   Updated: 2024/11/18 13:12:59 by yjinnouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 # define LIBFT_H
 
 # include <stdio.h> // size_t, NULL, printf
+# include <unistd.h> // write, read
 # include <stdlib.h> // size_t, NULL, malloc, free
 # include <limits.h> // INT_MAX, INT_MIN
-# include <unistd.h> // write
+# include <stdarg.h> // va_start, va_arg, va_copy, va_end
+# include <fcntl.h> // open, O_RDONLY
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// ----------------------------------------------------------------
+// libft - mandatory libc function
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -42,6 +50,7 @@ int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
+// libft - mandatory not in libc
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -54,6 +63,7 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
+// libft bonus
 typedef struct s_list
 {
 	void			*content;
@@ -69,5 +79,44 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+// ----------------------------------------------------------------
+// ft_printf
+int		ft_printf(const char *fmt, ...);
+int		ft_printf_format(char type, va_list args);
+int		ft_printf_char(int c);
+int		ft_printf_str(char *str);
+int		ft_printf_ptr(void *ptr);
+int		ft_printf_ptr_len(void *ptr);
+int		ft_printf_int(int nb);
+int		ft_printf_unsigned(unsigned long nb, int base, char *digits);
+
+// // for ft_printf bonus list
+// typedef struct s_flags
+// {
+// 	int	spec;
+// 	int	width;
+// 	int	left;
+// 	int	zero;
+// 	int	star;
+// 	int	precision;
+// 	int	hash;
+// 	int	space;
+// 	int	plus;
+// }		t_flags;
+
+// ----------------------------------------------------------------
+// get_next_line
+void	*gnl_calloc(size_t nmemb, size_t size);
+char	*gnl_parse_buff(char *buff, long length);
+char	*gnl_load_lines(long fd, char *buff);
+char	*get_next_line(long fd);
+
+// // for get_next_line bonus
+// typedef struct s_list
+// {
+// 	char			*str_buf;
+// 	struct s_list	*next;
+// }	t_list;
 
 #endif
